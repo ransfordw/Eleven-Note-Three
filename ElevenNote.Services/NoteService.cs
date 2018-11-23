0c2131737561bc2881c0499c51c5b77892093116
Ransford Walker <ransfordw@gmail.com>
2018-11-23 16:21:03 -0500

    Added note detail to modelsand controller

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -53,8 +53,28 @@ namespace ElevenNote.Services
                                     CreatedUtc = e.CreatedUtc
                                 }
                         );
-                
-                 return query.ToArray();
+
+                return query.ToArray();
+            }
+        }
+
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                    new NoteDetail
+                    {
+                        NoteId = entity.NoteId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
             }
         }
     }
--- ElevenNoteThree.WebMVC/Controllers/NoteController.cs
+++ ElevenNoteThree.WebMVC/Controllers/NoteController.cs
@@ -52,5 +52,13 @@ namespace ElevenNoteThree.WebMVC.Controllers
             var service = new NoteService(userId);
             return service;
         }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
     }
 }
