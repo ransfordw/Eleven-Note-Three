32fe8cd38d065662061f9625fb921ccf42032a14
Ransford Walker <ransfordw@gmail.com>
2018-11-23 09:51:30 -0500

    Add project files.

---
+++ ElevenNoteThree.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElevenNoteThree.WebMVC.MvcApplication" Language="C#" %>
---
+++ ElevenNoteThree.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNoteThree.WebMVC.Startup))]
+namespace ElevenNoteThree.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
