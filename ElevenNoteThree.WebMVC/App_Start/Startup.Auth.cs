99bb9c44c173e001544d6287d708f4319a1eebbf
Ransford Walker <ransfordw@gmail.com>
2018-11-23 10:00:57 -0500

    initial commit

--- ElevenNoteThree.WebMVC/App_Start/Startup.Auth.cs
+++ ElevenNoteThree.WebMVC/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using ElevenNoteThree.WebMVC.Models;
+using ElevenNote.Data;
 
 namespace ElevenNoteThree.WebMVC
 {
