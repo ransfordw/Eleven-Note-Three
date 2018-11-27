12860e6fd0fbc8b4ee95838e0735a4e6fc0ba424
Ransford Walker <ransfordw@gmail.com>
2018-11-26 20:19:11 -0500

    scaffolded webapi and updated refs and connection strings

---
+++ ElevenNoteThree.WebApi/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElevenNoteThree.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ ElevenNoteThree.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ElevenNoteThree.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
